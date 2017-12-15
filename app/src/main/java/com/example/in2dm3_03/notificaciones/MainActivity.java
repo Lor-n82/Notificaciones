9e1a87471b3d8ff4a57ea709858a5797e0954259
Loren Aparicio <loren.djkiller@gmail.com>
2017-12-15 13:43:39 +0100

    Segunda notificacion

--- app/src/main/java/com/example/in2dm3_03/notificaciones/MainActivity.java
+++ app/src/main/java/com/example/in2dm3_03/notificaciones/MainActivity.java
@@ -1,14 +1,17 @@
 package com.example.in2dm3_03.notificaciones;
 
+import android.annotation.TargetApi;
 import android.app.Notification;
 import android.app.NotificationChannel;
 import android.app.NotificationManager;
 import android.app.PendingIntent;
 import android.content.Intent;
+import android.os.Build;
 import android.support.v4.app.NotificationCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.RemoteViews;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
@@ -28,7 +31,7 @@ public class MainActivity extends AppCompatActivity {
         CharSequence text ="Notification ticket text";
         long when = System.currentTimeMillis();
 
-        NotificationCompat.Builder builder =new NotificationCompat.Builder(getApplicationContext(), NotificationChannel.DEFAULT_CHANNEL_ID);
+        NotificationCompat.Builder builder =new NotificationCompat.Builder(getApplicationContext());
         builder.setSmallIcon(icon);
         builder.setTicker(text);
         builder.setWhen(when);
@@ -51,7 +54,40 @@ public class MainActivity extends AppCompatActivity {
         finish();
     }
 
+    @TargetApi(Build.VERSION_CODES.HONEYCOMB)
     public void clickfin2(View v){
+        NotificationManager mNotificationManager = (NotificationManager) getSystemService(NOTIFICATION_SERVICE);
+
+        int icon = R.drawable.ic_launcher_background;
+        String text ="Notification ticket text";
+        long when = System.currentTimeMillis();
+        NotificationCompat.Builder builder =new NotificationCompat.Builder(getApplicationContext());
+        builder.setSmallIcon(icon);
+        builder.setTicker(text);
+        builder.setWhen(when);
+
+        RemoteViews contentView =new RemoteViews(getPackageName(),R.layout.activity_main2);
+        contentView.setImageViewResource(R.id.imageView, R.drawable.ic_launcher_foreground);
+        contentView.setTextViewText(R.id.textView2,"Personalizada");
+        contentView.setTextViewText(R.id.textView3,"Texto personalizado");
+        builder.setAutoCancel(true);
+        Intent intent =new Intent(this,Main2Activity.class);
+        PendingIntent contentIntent = PendingIntent.getActivity(this,0,intent,0);
+        builder.setContentIntent(contentIntent);
+        Notification notif;
+
+        if (Build.VERSION.SDK_INT >=Build.VERSION_CODES.N){
+            builder.setCustomContentView(contentView);
+            notif=builder.build();
+        }else{
+            notif = builder.build();
+            notif.contentView=contentView;
+        }
+
+        final int HELLO_ID=2;
+
+        mNotificationManager.notify(HELLO_ID, notif);
+        finish();
 
     }
 }
