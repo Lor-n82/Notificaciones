26a2efd34f15843676b82c4cec14ecd8683f96fe
Loren Aparicio <loren.djkiller@gmail.com>
2017-12-16 18:52:02 +0100

    Empezando a crear el toast personalizado

---
+++ app/src/main/java/com/example/in2dm3_03/notificaciones/ToastPersonalizado.java
@@ -0,0 +1,22 @@
+package com.example.in2dm3_03.notificaciones;
+
+import android.support.v4.widget.ImageViewCompat;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+
+import java.util.zip.Inflater;
+
+public class ToastPersonalizado extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        LayoutInflater inflater= getLayoutInflater();
+
+    }
+}
